9550de9e3b22d6b532647aa0fe23e1d8e6a10837
Vipin Singh Negi <vipinsinghnegi1996@gmail.com>
2018-10-05 18:23:27 +0530

    add node.cpp

---
+++ node.cpp
@@ -0,0 +1,28 @@
+#include<iostream.
+using namespace std;
+
+struct Node{
+Node * next;
+int data;
+}
+
+void print(){
+
+Node* temp= head;
+while(temp!=NULL)
+{
+
+cout<<temp->data<<"->";
+temp=temp->next;
+}
+
+cout<<endl;
+}
+
+Node* get_new_node(int data ){
+
+
+
+
+void add_last(int data){
+Node* new_node=
